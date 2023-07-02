65104029d594ce37f4bc6b975e4dd6c72b404977
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-07-02 12:36:38 +0300

    feat: review controller ready

---
+++ src/review/review.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { ReviewModel } from './review.model/review.model';
+
+
+@Controller('review')
+export class ReviewController {
+
+  @Post('create')
+  async create(
+    @Body() dto: Omit<ReviewModel, '_id'>
+  ) {
+
+  }
+
+  @Delete(':id')
+  async delete(
+    @Param('id') id: string
+  ) {
+
+  }
+
+
+  @Get('byProduct/:productId')
+  async getByProduct(
+    @Param('productId') productId: string
+  ) {
+
+  }
+
+
+}
