3f5652bf6b710969abbfb457c1b3fba76c83a980
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-07-06 18:01:22 +0300

    feat: user model ready

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './models/user.model';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async getByEmail(email: string) {
+    return await this.userModel.findOne({ email }).exec();
+  }
+
+  async createUser(email: string, password: string) {
+    const newUser = new this.userModel({ email, password });
+    return await newUser.save();
+  }
+}
