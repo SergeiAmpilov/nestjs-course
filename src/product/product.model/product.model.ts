612305078c2d2df9969ff488af5d69a44db533ba
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-07-24 15:12:35 +0300

    feat: product model

--- src/product/product.model/product.model.ts
+++ src/product/product.model/product.model.ts
@@ -1,17 +1,88 @@
-export class ProductModel {
-  _id: string;
-  image: string;
-  title: string;
-  price: number;
-  oldPrice: number;
-  credit: number;
-  calculatedRating: number;
-  description: string;
-  advantages: string;
-  disadvantages: string;
-  categories: string[];
-  tags: string[];
-  characteristics: {
-    [key: string]: string;
-  }
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, HydratedDocument, Schema as MSchema } from "mongoose";
+
+class ProductCharacteristic {
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  value: string;
 }
+
+const ProductCharacteristicSchema = SchemaFactory.createForClass(ProductCharacteristic);
+
+
+
+@Schema({
+  timestamps: true,
+})
+export class ProductModel extends Document {
+
+  _id: string;
+
+  @Prop({
+    type: MSchema.Types.String,
+  })
+  image: string;
+
+  @Prop({
+    type: MSchema.Types.String,
+  })
+  title: string;
+
+  @Prop({
+    type: MSchema.Types.Number,
+  })
+  price: number;
+
+  @Prop({
+    type: MSchema.Types.Number,
+  })
+  oldPrice: number;
+
+  @Prop({
+    type: MSchema.Types.Number,
+  })
+  credit: number;
+
+  @Prop({
+    type: MSchema.Types.Number,
+  })
+  calculatedRating: number;
+  
+  @Prop({
+    type: MSchema.Types.String,
+  })
+  description: string;
+
+  @Prop({
+    type: MSchema.Types.String,
+  })
+  advantages: string;
+
+  @Prop({
+    type: MSchema.Types.String,
+  })
+  disadvantages: string;
+
+  @Prop({
+    type: [MSchema.Types.String],
+  })
+  categories: string[];
+
+  @Prop({
+    type: [MSchema.Types.String],
+  })
+  tags: string[];
+
+  @Prop({
+    type: [ProductCharacteristicSchema],
+    _id: false,
+  })
+  characteristics: ProductCharacteristic[]
+
+}
+
+export const ProductSchema = SchemaFactory.createForClass(ProductModel);
+export type ProductDocument = HydratedDocument<ProductModel>;
