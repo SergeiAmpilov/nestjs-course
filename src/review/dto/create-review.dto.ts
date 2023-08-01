7f6c1bc62f4649fe00e850151f1e3a9b4a36e2ed
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-01 15:46:33 +0300

    feat: validate data

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,8 +1,20 @@
+import { IsString, IsInt, Max, Min} from 'class-validator';
 
 export class CreateReviewDto {
+  @IsString()
   name: string;  
+
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsInt()
+  @Max(5)
+  @Min(1)
   rating: number;
+
+  @IsString()
   productId: string;
 }
