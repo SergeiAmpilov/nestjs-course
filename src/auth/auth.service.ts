f4d6f33ba72626578af0dc31f1fe935dcf839f12
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-02 11:38:44 +0300

    feat: user registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserDocument, UserModel } from './user.model/user.model';
 import { Model } from 'mongoose';
+import { AuthDto } from './dto/auth.dto';
+import { getSalt, hash, genSalt } from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -9,10 +11,23 @@ export class AuthService {
     @InjectModel(UserModel.name) private readonly userModel: Model<UserDocument>
   ) {
     const a = this.userModel.findOne({});
+    // (await this.authModel.findOne().exec())   
+    
+  }
 
 
-    // (await this.authModel.findOne().exec())
-    
-    
+  async createUser(dto: AuthDto): Promise<UserDocument> {
+    const salt = await genSalt(10);
+    const passwordHash = await hash(dto.password, salt);
+    const newUser = new this.userModel({
+      email: dto.login,
+      passwordHash,
+    });
+    return newUser.save();
+
+  }
+
+  async findUser(email: string) {
+    return this.userModel.findOne({ email }).exec();
   }
 }
