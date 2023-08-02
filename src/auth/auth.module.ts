d93e6ec23e669c78537db8d2895fad0395bf1dd3
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-02 12:57:11 +0300

    feat: login ready

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModel, UserSchema } from './user.model/user.model';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJwtConfig } from './configs/jwt.config';
 
 
 @Module({
@@ -13,6 +16,11 @@ import { AuthService } from './auth.service';
         schema: UserSchema
       },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService]
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException,  } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserDocument, UserModel } from './user.model/user.model';
 import { Model } from 'mongoose';
 import { AuthDto } from './dto/auth.dto';
-import { getSalt, hash, genSalt } from 'bcryptjs';
+import {  hash, genSalt, compare } from 'bcryptjs';
+import { USER_NOT_FOUND_ERROR, WRONG_PASSWORD_ERROR } from './auth.constants';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectModel(UserModel.name) private readonly userModel: Model<UserDocument>
+    @InjectModel(UserModel.name) private readonly userModel: Model<UserDocument>,
+    private readonly jwtService: JwtService,
   ) {
     const a = this.userModel.findOne({});
     // (await this.authModel.findOne().exec())   
@@ -29,5 +32,32 @@ export class AuthService {
 
   async findUser(email: string) {
     return this.userModel.findOne({ email }).exec();
+  }
+
+  async validateUser(email: string, password: string): Promise<Pick<UserModel, 'email'>> {
+
+    const user = await this.findUser(email);
+
+    if (!user) {
+      throw new UnauthorizedException(USER_NOT_FOUND_ERROR);
+    } 
+    
+    const isCorrectPassword = await compare(password, user.passwordHash);
+
+    if (!isCorrectPassword) {
+      throw new UnauthorizedException(WRONG_PASSWORD_ERROR);
+    }
+
+    return { email: user.email };    
+
+  }
+
+
+  async login(email: string): Promise<{ access_token: string }> {
+    const payload = { email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    }
+
   }
 }
