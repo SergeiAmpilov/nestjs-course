1acfe454f454fa52ce8f87f256957f5556d152de
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-02 18:30:27 +0300

    feat: tests with auth

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,6 +7,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 
 
 @Controller('review')
@@ -39,12 +40,13 @@ export class ReviewController {
     }
   }
 
-
+  
   @Get('byProduct/:productId')
   async getByProduct(
-    @Param('productId') productId: string
+    @Param('productId') productId: string,
   ) {
 
+  
     return this.reviewService.findByProductId(productId);
 
   }
