e08be9c6bf5426ede4870be1b556d23aea62dfa4
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-07 16:58:42 +0300

    feat: serve static

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+import { path } from 'app-root-path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
+  imports: [ServeStaticModule.forRoot({
+    rootPath: `${path}/uploads`,
+    serveRoot: `/static`
+  })],
   controllers: [FilesController],
   providers: [FilesService]
 })
