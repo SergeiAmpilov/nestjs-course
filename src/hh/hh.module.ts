10d52abd66066514abd4ae5aee3a4e8f88869752
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-12 10:23:29 +0300

    feat: hh module

--- src/hh/hh.module.ts
+++ src/hh/hh.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { HhService } from './hh.service';
 import { TopPageModule } from 'src/top-page/top-page.module';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   providers: [HhService],
-  imports: [TopPageModule]
+  imports: [
+    TopPageModule,
+    ConfigModule,
+    HttpModule
+  ],
 })
 export class HhModule {}
--- src/hh/hh.service.ts
+++ src/hh/hh.service.ts
@@ -1,4 +1,30 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { API_URL } from './hh.constants';
 
 @Injectable()
-export class HhService {}
+export class HhService {
+  token: string;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {
+    this.token = this.configService.get('HH_TOKEN') ?? '';
+  }
+
+
+  async getData(text: string) {
+    const res = await this.httpService.get(API_URL.vacancies, {
+      params: {
+        text,
+        cluster: true
+      },
+      headers: {
+        'User-Agent': 'OwlTop/1.0 (antonlaricher@gmail.com)',
+        Authorization: 'Bearer ' + this.token,
+      }
+    }).toPromise();
+  }
+}
